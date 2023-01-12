fa17a6193249c335cdf751efb1b37a79104e12b2
Felix Schulze Sindern <felixs@fh-muenster.de>
2023-01-12 18:28:39 +0100

    Add german locals

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,69 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
   logo: <span>Sudoku Generator</span>,
   project: {
-    link: 'https://git.fh-muenster.de/spqr-2022/spqr-sudoku-generator',
+    link: 'https://github.com/FelixSchuSi/sudoku-generator',
   },
-  docsRepositoryBase: 'https://git.fh-muenster.de/spqr-2022/spqr-sudoku-generator',
+  docsRepositoryBase: 'https://github.com/FelixSchuSi/sudoku-generator/tree/main/',
   footer: {
     text: 'Nextra Docs Template',
   },
+  editLink: {
+    text: () => {
+      const { locale } = useRouter();
+      switch (locale) {
+        case "de":
+        default:
+          return <p>Bearbeite diese Seite auf GitHub →</p>
+      }
+    },
+  },
+  feedback: {
+    content: () => {
+      const { locale } = useRouter();
+      switch (locale) {
+        case "de":
+        default:
+          return <p>Frage? Gib uns Feedback →</p>
+      }
+    },
+  },
+  toc: {
+    title: () => {
+      const { locale } = useRouter();
+      switch (locale) {
+        case "de":
+        default:
+          return <>Auf dieser Seite</>
+      }
+    },
+  },
+  search: {
+    placeholder: () => {
+      const { locale } = useRouter();
+      switch (locale) {
+        case "de":
+        default:
+          return "Suchen..."
+      }
+    },
+  },
+  gitTimestamp: ({ timestamp }) => {
+    const { locale } = useRouter();
+    console.log(timestamp);
+    switch (locale) {
+      case "de":
+      default:
+        return <>Zuletzt aktualisiert am {new Intl.DateTimeFormat('de', { dateStyle: 'long' }).format(timestamp)}</>
+    }
+  },
+  i18n: [
+    { locale: 'de', text: 'Deutsch' }
+  ],
+
 }
 
 export default config
