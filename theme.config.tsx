430e03644b203599b50535d43c461571ce89fb52
Felix Schulze Sindern <felixs@fh-muenster.de>
2023-01-12 14:40:37 +0100

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Sudoku Generator</span>,
+  project: {
+    link: 'https://git.fh-muenster.de/spqr-2022/spqr-sudoku-generator',
+  },
+  docsRepositoryBase: 'https://git.fh-muenster.de/spqr-2022/spqr-sudoku-generator',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
